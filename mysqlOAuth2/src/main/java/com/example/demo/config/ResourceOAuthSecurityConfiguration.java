8f3c6e4dbaade8129d66c61047a371b5e96450ac
Maxim Mamuta <Maxim@MacBook-Pro-macbook.local>
2017-09-24 18:33:31 +0300

    mysql with OAuth2 security

---
+++ mysqlOAuth2/src/main/java/com/example/demo/config/ResourceOAuthSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+
+/**
+ * Created by maxmamuta on 9/18/17.
+ */
+@Configuration
+@EnableAuthorizationServer
+@EnableResourceServer
+public class ResourceOAuthSecurityConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/**").authenticated();
+    }
+}
